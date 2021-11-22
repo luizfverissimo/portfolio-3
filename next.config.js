a533ed35bce0f541c15969e6ec2b76d4277781bf
Luiz Fernando Veríssimo <62751844+luizfverissimo@users.noreply.github.com>
2021-11-21 21:39:52 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/links',
+        destination: 'https://lfv-bio-link.vercel.app/',
+        permanent: true,
+      },
+    ]
+  },
 }
